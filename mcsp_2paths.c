f46b1824435949e0b7c44a00bf82fdffc13071b8
James Trimble <james.trimble@yahoo.co.uk>
2017-07-19 10:51:23 +0100

    Tweak

--- mcsp_2paths.c
+++ mcsp_2paths.c
@@ -367,7 +367,7 @@ std::pair<int, int> select_bidomain_and_branching_var(const vector<Bidomain>& do
     for (unsigned int k=0; k<bidomain_order.size(); k++) {
         int i = bidomain_order[k];
         const Bidomain &bd = domains[i];
-        if (bd.right_len > 25 && best_score.first != INT_MAX)
+        if (best_score.first != INT_MAX && bd.right_len > best_score.first * 3)  // TODO: check if there's something better than 3
             break;
         auto score = bidomain_score(bd, left, right, g0_2p, g1_2p, g0_deg, g1_deg, current, best_score);
         if (score < best_score) {
