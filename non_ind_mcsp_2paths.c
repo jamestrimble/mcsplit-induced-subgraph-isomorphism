d648da2d9bff7fb568f76a26306ba43332b0527d
James Trimble <james.trimble@yahoo.co.uk>
2017-07-26 11:27:53 +0100

    Better all-diff propagation, possibly fixing bugs

--- non_ind_mcsp_2paths.c
+++ non_ind_mcsp_2paths.c
@@ -222,20 +222,18 @@ bool propagate_alldiff(vector<Bidomain>& domains,
         const Graph& g0, const Graph& g1, vector<int>& left)
 {
     // These are really vectors of boolean values
-    vector<unsigned char> vv0(g0.n);
-    vector<unsigned char> vv1(g1.n);
-    int vv0_count = 0;
-    int vv1_count = 0;
+    vector<unsigned char> vv0(g0.n, 0);
+    vector<unsigned char> vv1(g1.n, 0);
 
     int x = -1;
+    int vv0_count = 0;
+    int vv1_count = 0;
     for (int i=0; i<(int)domains.size(); i++) {
         auto& bd = domains[i];
         for (int j=bd.l; j<bd.l + bd.left_len; j++) {
             int v = left[j];
-            if (!vv0[v]) {
-                vv0[v] = true;
-                vv0_count++;
-            }
+            vv0[v] = true;
+            vv0_count++;
         }
         for (int w : bd.right_set) {
             if (!vv1[w]) {
@@ -245,38 +243,41 @@ bool propagate_alldiff(vector<Bidomain>& domains,
         }
         if (vv0_count > vv1_count)
             return false;
-        else if (x==-1 && vv0_count==vv1_count)
+        else if (vv0_count==vv1_count)
             x = i;
     }
     if (x > -1) {
+        vv0_count = 0;
+        vv1_count = 0;
         for (int i=0; i<g0.n; i++) vv0[i] = 0;
         for (int i=0; i<g1.n; i++) vv1[i] = 0;
-        for (int i=0; i<=x; i++) {
+        std::vector<unsigned char> ww_to_erase(g1.n, 0);
+        bool there_are_ww_to_erase = false;
+        for (int i=0; i<(int)domains.size(); i++) {
             auto& bd = domains[i];
-            for (int j=bd.l; j<bd.l + bd.left_len; j++) {
-                int v = left[j];
-                vv0[v] = true;
+            if (there_are_ww_to_erase) {
+                bd.right_set.erase(std::remove_if(bd.right_set.begin(), bd.right_set.end(), 
+                            [&](int w){ return ww_to_erase[w]; }),
+                        bd.right_set.end());
             }
-            for (int w : bd.right_set) {
-                vv1[w] = true;
-            }
-        }
-        for (int i=x+1; i<(int)domains.size(); i++) {
-            auto& bd = domains[i];
-
-            int k=bd.l;
-            for (int j=bd.l; j<bd.l+bd.left_len; j++) {
-                int v = left[j];
-                if (vv0[v]) {
-                    std::swap(left[k], left[j]);
-                    k++;
+            if (i <= x) {
+                for (int j=bd.l; j<bd.l + bd.left_len; j++) {
+                    int v = left[j];
+                    vv0[v] = true;
+                    vv0_count++;
+                }
+                for (int w : bd.right_set) {
+                    if (!vv1[w]) {
+                        vv1[w] = true;
+                        vv1_count++;
+                    }
+                }
+                if (vv0_count == vv1_count) {
+                    there_are_ww_to_erase = true;
+                    for (int i=0; i<g1.n; i++)
+                        ww_to_erase[i] = vv1[i];
                 }
             }
-            bd.l = k;
-
-            bd.right_set.erase(std::remove_if(bd.right_set.begin(), bd.right_set.end(), 
-                        [&](int w){ return vv1[w]; }),
-                    bd.right_set.end());
         }
     }
     return true;
